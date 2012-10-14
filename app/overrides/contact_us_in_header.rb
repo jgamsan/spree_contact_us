24236b2ae0df9d2355eddf26690316d5360ff0e5
jose miguel <miguel.gamazo@galiclick.com>
2012-10-14 19:08:18 +0200

    Mas mejoras

--- app/overrides/contact_us_in_header.rb
+++ app/overrides/contact_us_in_header.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "contact_us_in_header",
                      :insert_bottom => "#main-nav-bar",
                      :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/contact-us') ? 'current' : 'not'%>'><%= link_to t('spree.contact_us.contacts.new.contact_us'), '/contact-us'  %></li>
-                     <li class='<%= (request.fullpath.gsub('//','/') == '/about') ? 'current' : 'not'%>'><%= link_to t('spree.about'), '/about_index'  %></li>"
+                     <li class='<%= (request.fullpath.gsub('//','/') == '/about') ? 'current' : 'not'%>'><%= link_to t('spree.about'), '/about'  %></li>"
                      )
